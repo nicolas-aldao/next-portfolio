33b5441ace84b985d5aa8b314b2a6eec64b31b2b
Nicolas Aldao <nicolasaldao@Nicolass-MacBook-Pro.local>
2023-02-01 18:15:12 -0300

    some changes 6

--- components/UI/About.jsx
+++ components/UI/About.jsx
@@ -68,46 +68,61 @@ const About = () => {
               </button>
             </div>
           </Col>
-          <Col lg="6" className={`${classes.about__content}`}>
-          <section className={`${classes.tecnologies_container}`}>
-            <div className={`${classes.skill_container} ${classes.red_border}`}>
-              <HtmlIcon />
-              <BadgeAtom key="Html" className={`${classes.skills_badge}`}>
-                Html
-              </BadgeAtom>
-            </div>
-            <div className={`${classes.skill_container} ${classes.red_border}`}>
-              <CssIcon />
-              <BadgeAtom key="Css" className={`${classes.skills_badge}`}>
-                Css
-              </BadgeAtom>
-            </div>
-            <div className={`${classes.skill_container} ${classes.red_border}`}>
-              <JsIcon />
-              <BadgeAtom key="Javascript" className={`${classes.skills_badge}`}>
-                Javascript
-              </BadgeAtom>
-            </div>
-            <div className={`${classes.skill_container} ${classes.red_border}`}>
-              <ReactIcon />
-              <BadgeAtom key="React" className={`${classes.skills_badge}`}>
-                React
-              </BadgeAtom>
-            </div>
-            <div className={`${classes.skill_container} ${classes.red_border}`}>
-              <NextJsIcon />
-              <BadgeAtom key="Next.js" className={`${classes.skills_badge}`}>
-                Next.js
-              </BadgeAtom>
-            </div>
-            <div className={`${classes.skill_container} ${classes.red_border}`}>
-              <GitIcon />
-              <BadgeAtom key="Git" className={`${classes.skills_badge}`}>
-                Git
-              </BadgeAtom>
-            </div>
-          </section>
-      </Col>
+          <Col lg="6" xs="10" className={`${classes.about__content}`}>
+            <section className={`${classes.tecnologies_container}`}>
+              <div
+                className={`${classes.skill_container} ${classes.red_border}`}
+              >
+                <HtmlIcon />
+                <BadgeAtom key="Html" className={`${classes.skills_badge}`}>
+                  Html
+                </BadgeAtom>
+              </div>
+              <div
+                className={`${classes.skill_container} ${classes.red_border}`}
+              >
+                <CssIcon />
+                <BadgeAtom key="Css" className={`${classes.skills_badge}`}>
+                  Css
+                </BadgeAtom>
+              </div>
+              <div
+                className={`${classes.skill_container} ${classes.red_border}`}
+              >
+                <JsIcon />
+                <BadgeAtom
+                  key="Javascript"
+                  className={`${classes.skills_badge}`}
+                >
+                  Javascript
+                </BadgeAtom>
+              </div>
+              <div
+                className={`${classes.skill_container} ${classes.red_border}`}
+              >
+                <ReactIcon />
+                <BadgeAtom key="React" className={`${classes.skills_badge}`}>
+                  React
+                </BadgeAtom>
+              </div>
+              <div
+                className={`${classes.skill_container} ${classes.red_border}`}
+              >
+                <NextJsIcon />
+                <BadgeAtom key="Next.js" className={`${classes.skills_badge}`}>
+                  Next.js
+                </BadgeAtom>
+              </div>
+              <div
+                className={`${classes.skill_container} ${classes.red_border}`}
+              >
+                <GitIcon />
+                <BadgeAtom key="Git" className={`${classes.skills_badge}`}>
+                  Git
+                </BadgeAtom>
+              </div>
+            </section>
+          </Col>
         </Row>
       </Container>
     </section>
