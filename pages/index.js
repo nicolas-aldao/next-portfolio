82074343bb32df30eb1add8616f47fd8b04b80e6
Nicolas Aldao <nicolasaldao@Nicolass-MBP.fibertel.com.ar>
2023-08-20 00:41:22 -0300

    added metadata for preview on sharing url

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,41 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Nicolás Aldao</title>
+        <title>Nicolás Aldao | Frontend Developer</title>
+        <meta charset="utf-8" />
+        {/* <meta http-equiv="X-UA-Compatible" content="IE=edge" /> */}
+        <meta name="csrf_token" content="" />
+        <meta property="type" content="website" />
+        <meta property="url" content={"currentUrl"} />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="_token" content="" />
+        <meta name="robots" content="noodp" />
+        <meta property="title" content={"title"} />
+        <meta property="quote" content={"quote"} />
+        <meta name="description" content={"description"} />
+        <meta
+          property="image"
+          content={"https://64.media.tumblr.com/avatar_f332fc701bb2_128.pnj"}
+        />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={"title"} />
+        <meta property="og:quote" content={"quote"} />
+        <meta property="og:hashtag" content={"hashtag"} />
+        <meta
+          property="og:image"
+          content={"https://64.media.tumblr.com/avatar_f332fc701bb2_128.pnj"}
+        />
+        <meta content="image/*" property="og:image:type" />
+        <meta property="og:url" content={"currentUrl"} />
+        <meta property="og:site_name" content="CampersTribe" />
+        <meta property="og:description" content={"description"} />
       </Head>
       <Hero />
       {/* <Services /> */}
