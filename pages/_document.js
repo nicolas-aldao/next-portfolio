9c35ecd24f43c3a31f2c8618b510b0fc12783484
Nicolas Aldao <nicolasaldao@Nicolass-MBP.fibertel.com.ar>
2024-02-01 23:45:20 -0300

    testing document file

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,32 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <meta
+            name="description"
+            content="Checkout our cool page"
+            key="desc"
+          />
+          <meta property="og:title" content="Social Title for Cool Page" />
+          <meta
+            property="og:description"
+            content="And a social description for our cool page"
+          />
+          <meta
+            property="og:image"
+            content="https://example.com/images/cool-page.jpg"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Onest:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
